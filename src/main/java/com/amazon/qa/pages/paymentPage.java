31c4289942962bb50a8af49e25e631967d1124f1
Hiral Vaghela <hiralvaghela@Hirals-MacBook-Air.local>
2022-08-05 19:16:29 -0400

    update

---
+++ src/main/java/com/amazon/qa/pages/paymentPage.java
@@ -0,0 +1,11 @@
+package com.amazon.qa.pages;
+
+import com.amazon.qa.base.TestBase;
+
+public class paymentPage extends TestBase{
+
+	private void payment() {
+		// TODO Auto-generated method stub
+			System.out.println("payment page is coming soon");
+	}
+}
